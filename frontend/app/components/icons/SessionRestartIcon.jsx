8ff208a4386ef7258f6cb86002014dcb35be203c
Victor Cretu <eaa24vac@students.eaaa.dk>
2025-12-10 21:34:17 +0100

    Add dark theme support and update icon stroke colors

--- frontend/app/components/icons/SessionRestartIcon.jsx
+++ frontend/app/components/icons/SessionRestartIcon.jsx
@@ -11,14 +11,14 @@ function SessionRestartIcon() {
     >
       <path
         d="M31.875 31.875H15C11.875 31.875 5.625 30 5.625 22.5C5.625 15 11.875 13.125 15 13.125H30C33.125 13.125 39.375 15 39.375 22.5C39.375 25.3019 38.5027 27.3187 37.2469 28.746"
-        stroke="#7D7994"
+        stroke="var(--text-color-light)"
         strokeWidth="2.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M27.1875 27.1875L31.875 31.875L27.1875 36.5625"
-        stroke="#7D7994"
+        stroke="var(--text-color-light)"
         strokeWidth="2.5"
         strokeLinecap="round"
         strokeLinejoin="round"
